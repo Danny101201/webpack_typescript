02d3ad2e2ccca84e12f1f3e3e726417d26e0bb46
AN0030 <an0030@AN0030deMacBook-Pro.local>
2022-07-18 18:05:13 +0800

    feat(page)

--- src/_types_/fetchData.d.ts
+++ src/_types_/fetchData.d.ts
@@ -25,3 +25,17 @@ type UBikeInfo = {
   regionName: string
   stopName: string
 }
+type districsName =
+  '中正區' |
+  '大同區' |
+  '中山區' |
+  '松山區' |
+  '大安區' |
+  '萬華區' |
+  '信義區' |
+  '士林區' |
+  '北投區' |
+  '內湖區' |
+  '南港區' |
+  '文山區' |
+  '臺大專區'
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
-import L, { LatLngExpression } from 'leaflet'
+import L, { LatLngExpression, LayerGroup } from 'leaflet'
 import fetchBikeData from './fetchData'
 import mapConfig from './map.config'
+import { districs } from './districtData'
+const SelectDistrict = <HTMLSelectElement>(document.querySelector('#select-district'))
+districs.forEach(d => {
+  const optionTag = document.createElement('option')
+  optionTag.setAttribute('value', d)
+  optionTag.innerHTML = d
+  SelectDistrict?.appendChild(optionTag)
+})
+let makerLayer: LayerGroup
+
 const {
   taipeiCoor,
   zoom,
@@ -10,7 +20,35 @@ const {
 
 var map = L.map(mapType).setView(taipeiCoor, zoom);
 L.tileLayer(tileLayerUrl).addTo(map);
-fetchBikeData().then(res => {
-  console.log(res);
+let currentDistrict = SelectDistrict.value as districsName
+function updateUBikeMap(districs: districsName): void {
+  fetchBikeData().then(data => {
+    const selectData = data.filter(info => info.regionName === currentDistrict)
+    const marker = selectData.map(data => {
+      const marker = new L.Marker(data.lanLang)
+      marker.bindTooltip(`
+      <p>${data.regionName} - ${data.stopName}</p>
+      <p>總自行車數 : ${data.totalBikes}</p>
+      <p>可用自行車數 : ${data.availableBikes}</p>
+      `)
+      marker.on('mouseover', () => {
+        marker.openTooltip()
+      })
+      marker.on('mouseleave', () => {
+        marker.closeTooltip()
+      })
+      return marker
+    })
+    makerLayer = L.layerGroup(marker)
+    makerLayer.addTo(map)
+  })
+}
+updateUBikeMap(currentDistrict)
+SelectDistrict.addEventListener('change', (e: any) => {
+  currentDistrict = e.target.value
+  makerLayer.remove()
+  updateUBikeMap(currentDistrict)
 })
+
+
 
